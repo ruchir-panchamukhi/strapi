1a6ece9a8e36fe029f79541aec02234daa1b1aab
Ruchir R Panchamukhi <ruchir.panchamukhi@Triconinfotech.com>
2025-01-21 23:00:04 +0530

    added a strapi files

--- config/plugins.js
+++ config/plugins.js
@@ -25,20 +25,3 @@ module.exports = ({ env }) => ({
     }
   });
 
-  export default ({ env }) => ({
-    upload: {
-        config: {
-            provider: 'aws-s3',
-            providerOptions: {
-                s3Options: {
-                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                    secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    region: env('AWS_REGION'),
-                    params: {
-                      Bucket: env('AWS_BUCKET_NAME'),
-                    },
-                }
-            },
-        },
-    }
-  });
