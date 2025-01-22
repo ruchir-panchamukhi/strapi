1528f952cd5eb1c5cc81d645605159ee401933db
Ruchir R Panchamukhi <ruchir.panchamukhi@Triconinfotech.com>
2025-01-22 12:43:39 +0530

    made changes

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "database-1.cvcgaoqs88cl.us-west-1.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME", "database-1"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Ruchir00123"),
     },
     useNullAsDefault: true,
   },
